23c1a00abaa9c5722836892a550d8f543d790947
Meow <sharmainemanijado@gmail.com>
2017-09-08 23:57:03 +0800

    query reduction

--- app/Thread.php
+++ app/Thread.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Thread extends Model
 {
     protected $guarded = [];
+    protected $with = ['creator'];
 
     protected static function boot()
     {
@@ -24,9 +25,7 @@ class Thread extends Model
 
     public function replies()
     {
-    	return $this->hasMany(Reply::class)
-            ->withCount('favorites')
-            ->with('owner');
+    	return $this->hasMany(Reply::class);
     }
 
     public function creator()
