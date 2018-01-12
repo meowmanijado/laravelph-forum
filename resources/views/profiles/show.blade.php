8fe26124a251bc90bbc050eae7e66c272795aaf0
Meow <sharmainemanijado@gmail.com>
2018-01-13 00:23:45 +0800

    avatar upload

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -5,20 +5,9 @@
 		<div class="row">
 			<div class="col-md-8 col-md-offset-2">
 				<div class="page-header">
-					<h1> {{ $profileUser->name }} </h1>
-
-					@can ('update', $profileUser)
-						<form method="POST" action="{{ route('avatar', $profileUser) }}" enctype="multipart/form-data">
-							{{ csrf_field() }}
-							<input type="file" name="avatar">
-
-							<button type="submit" class="btn btn-primary">Add Avatar</button>
-						</form>
-					@endcan
-
-					<img src="{{ asset($profileUser->avatar_path) }}"  width="50" height="50">
+					<avatar-form :user="{{ $profileUser }}"></avatar-form>
 				</div>
-
+				
 				@forelse ($activities as $date => $activity)
 					<h3 class="page-header">{{ $date }} </h3>
 
