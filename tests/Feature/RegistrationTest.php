126b18458b5c4c96440d964dc928badbafa363bd
Meow <sharmainemanijado@gmail.com>
2018-01-19 00:21:35 +0800

    users must confirm their email address

---
+++ tests/Feature/RegistrationTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Auth\Events\Registered;
+use App\Mail\PleaseConfirmYourEmail;
+use App\User;
+
+class RegistrationTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test  */
+    public function a_confirmation_mail_is_sent_upon_registration()
+    {
+        Mail::fake();
+
+        event(new Registered(create('App\User')));
+
+        Mail::assertSent(PleaseConfirmYourEmail::class);
+    }
+
+    /** @test  */
+    public function user_can_fully_confirm_their_email_addresses()
+    {
+        $this->post('/register', [
+            'name' => 'John',
+            'email' => 'john@example.com',
+            'password' => 'foobar',
+            'password_confirmation' => 'foobar'
+        ]);
+
+        $user = User::whereName('John')->first();
+
+        $this->assertFalse($user->confirmed);
+
+        $this->assertNotNull($user->confirmation_token);
+
+        $response = $this->get('/register/confirm?token=' . $user->confirmation_token);
+
+        $this->assertTrue($user->fresh()->confirmed);
+
+        $response->assertRedirect('/threads');
+    }
+}
