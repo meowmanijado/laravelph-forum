967a0b1464836a3e283ef8bcd1ac868233159dcb
Meow <sharmainemanijado@gmail.com>
2018-01-20 01:22:27 +0800

    unique slug

--- app/Thread.php
+++ app/Thread.php
@@ -54,7 +54,7 @@ class Thread extends Model
      */
     public function path()
     {
-        return "/threads/{$this->channel->slug}/{$this->id}";
+        return "/threads/{$this->channel->slug}/{$this->slug}";
     }
 
     /**
@@ -169,5 +169,10 @@ class Thread extends Model
         $key = $user->visitedThreadCacheKey($this);
 
         return $this->updated_at > cache($key);
+    }
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
     }
 }
