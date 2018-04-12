46f9030935adbaeb4ea86e55f6cb97c812f24f26
meow <sharmainemanijado@gmail.com>
2018-04-12 23:41:33 +0800

    admin may lock any thread

--- public/js/app.js
+++ public/js/app.js
@@ -57963,6 +57963,9 @@ module.exports = {
         var prop = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 'user_id';
 
         return model[prop] === user.id;
+    },
+    isAdmin: function isAdmin() {
+        return ['JohnDoe', 'JaneDoe'].includes(user.name);
     }
 };
 
@@ -59136,11 +59139,12 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 
 
 /* harmony default export */ __webpack_exports__["default"] = ({
-	props: ['initialRepliesCount'],
+	props: ['dataRepliesCount', 'dataLocked'],
 	components: { Replies: __WEBPACK_IMPORTED_MODULE_0__components_Replies_vue___default.a, SubscribeButton: __WEBPACK_IMPORTED_MODULE_1__components_SubscribeButton_vue___default.a },
 	data: function data() {
 		return {
-			repliesCount: this.initialRepliesCount
+			repliesCount: this.dataRepliesCount,
+			locked: this.dataLocked
 		};
 	}
 });
@@ -59202,6 +59206,9 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__NewReply_vue__ = __webpack_require__(188);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__NewReply_vue___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1__NewReply_vue__);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__mixins_collection__ = __webpack_require__(193);
+//
+//
+//
 //
 //
 //
@@ -61851,7 +61858,13 @@ var render = function() {
         on: { changed: _vm.fetch }
       }),
       _vm._v(" "),
-      _c("new-reply", { on: { created: _vm.add } })
+      _vm.$parent.locked
+        ? _c("p", [
+            _vm._v(
+              "\n\t\tThis thread has been locked. No more replies are allowed.\n\t"
+            )
+          ])
+        : _c("new-reply", { on: { created: _vm.add } })
     ],
     2
   )
--- resources/assets/js/pages/Thread.vue
+++ resources/assets/js/pages/Thread.vue
@@ -3,11 +3,12 @@
 	import SubscribeButton from '../components/SubscribeButton.vue';
 
 	export default {
-		props: ['initialRepliesCount'],
+		props: ['dataRepliesCount','dataLocked'],
 		components: { Replies, SubscribeButton },
 		data() {
 			return {
-				repliesCount: this.initialRepliesCount
+				repliesCount: this.dataRepliesCount,
+				locked: this.dataLocked
 			}
 		}
 	}
